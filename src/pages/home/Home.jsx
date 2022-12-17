183332ba1fc0a0fe6b7489f382c07fa0683fb8f7
jam-dev <jamalvga2002@gmail.com>
2022-12-17 13:36:49 +0530

    home page edited

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import Sidebar from '../../components/sidebar/Sidebar';
 import "./home.css";
 
 
@@ -7,6 +8,7 @@ import "./home.css";
 export default function home() {
   return (
     <div className='home'>
+      <Sidebar />
       <FeaturedInfo />
       
     </div>
