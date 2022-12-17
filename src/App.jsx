51f57e59b3129d193d6bea987acdbb162f3c0b30
jam-dev <jamalvga2002@gmail.com>
2022-12-17 12:09:10 +0530

    login page created

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+
+import Navbar from "./components/topbar/topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Login from "./pages/Signup/Login";
+import SignupPage from "./pages/Signup/Signin";
+import Home from "./pages/home/Home";
+import React from "react";
+import "./app.css";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+import { useState } from "react";
+import Landingpage from "./components/Landingpage.js/Landingpage";
+import { Dashboard } from "@material-ui/icons";
+
+function App() {
+  
+  const [auth, setAuth] = useState(
+    false || window.localStorage.getItem("auth") === "true"
+  );
+
+  const navigate = useNavigate();
+
+
+
+
+  return (
+    <>
+      <div className="App">
+     
+          {auth && <Sidebar />}
+          <Routes>
+            {/* <Route path="/*" element={<Landingpage />} /> */}
+            {auth && <Route  path="/home" element={<Home />} />}
+            {!auth ? (
+              <Route exact path="/" element={<SignupPage />} />
+            ) : (
+              <Route exact path="/" element={<Home />} />
+            )}
+
+            {}
+            {!auth && <Route  path="/login" element={<Login />} />}
+            
+          </Routes>
+        </div>
+    </>
+
+  );
+}
+export default App;
+
+
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import "./home.css";
+
+
+
+export default function home() {
+  return (
+    <div className='home'>
+      <FeaturedInfo />
+      
+    </div>
+
+  )
+}
